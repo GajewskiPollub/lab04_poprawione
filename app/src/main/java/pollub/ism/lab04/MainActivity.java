bec0da889d2930484f31ef657c5642f858e48bc5
patry <patryk.gajewski@pollub.edu.pl>
2021-03-25 00:17:54 +0100

    Lab4_poprawione

--- app/src/main/java/pollub/ism/lab04/MainActivity.java
+++ app/src/main/java/pollub/ism/lab04/MainActivity.java
@@ -1,5 +1,6 @@
 package pollub.ism.lab04;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     char first_player = 'X';
     char second_player = 'O';
     int turn_number = 1;
+    String[] saveStatus = new String[9];
+    boolean[] saveView = {true,true,true,true,true,true,true,true,true};
 /*
     public boolean check_row() {
         if ((table[0][0] == 'X' && table[0][1] == 'X' && table[0][2] == 'X') || (table[0][0] == 'O' && table[0][1] == 'O' && table[0][2] == 'O')) {
@@ -157,19 +160,54 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button button;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                button = (Button) findViewById(getResources().getIdentifier("button_" + i + "_"+j, "id", this.getPackageName()));
+                button.setText(saveStatus[i*3+j]);
+                button.setEnabled(saveView[i*3+j]);
+                if (saveStatus[i*3+j]!=null){
+                    table[i][j]=saveStatus[i*3+j].charAt(0);
+                }
+            }
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArray("status_gry",saveStatus);
+        outState.putBooleanArray("blokowanie_pola",saveView);
+        outState.putInt("aktualny_symbol", turn_number);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        saveStatus =savedInstanceState.getStringArray("status_gry");
+        saveView =savedInstanceState.getBooleanArray("blokowanie_pola");
+        turn_number = savedInstanceState.getInt("aktualny_symbol");
+    }
     public void kliknieciePrzycisku(View view) {
         String[] split = (view.getResources().getResourceEntryName(view.getId())).split("_");
         short row_number = Short.parseShort(split[1]);
         short column_number = Short.parseShort(split[2]);
             if (turn_number % 2 == 1) {
                 table[row_number][column_number] = first_player;
+                saveStatus[(row_number)*3+(column_number)] = Character.toString(first_player);
             } else {
                 table[row_number][column_number] = second_player;
+                saveStatus[(row_number)*3+(column_number)] = Character.toString(second_player);
             }
             Button button = (Button) findViewById(view.getId());
             button.setText(Character.toString(table[row_number][column_number]));
             button.setEnabled(false);
-            check_winner();
+            saveView[row_number*3+column_number]=false;
+
+        check_winner();
         TextView textView2=(TextView)findViewById(R.id.result);
        if((turn_number==9 )&&(check_winner()==false))
         {
